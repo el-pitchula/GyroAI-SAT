8353c00f469fed623d5c2d686664134559bfbe25
Jay Jay <64316338+el-pitchula@users.noreply.github.com>
2025-04-14 20:41:00 -0300

    main

--- simulation/quaternions.py
+++ simulation/quaternions.py
@@ -1,6 +1,19 @@
 #from manim_imports_ext import *
 # pip install manimlib
+from manimlib import *
+from manimlib.mobject.svg.old_tex_mobject import *
 
+from custom.backdrops import *
+from custom.banner import *
+from custom.characters.pi_creature import *
+from custom.characters.pi_creature_animations import *
+from custom.characters.pi_creature_scene import *
+from custom.deprecated import *
+from custom.drawings import *
+from custom.end_screen import *
+from custom.filler import *
+from custom.logo import *
+from custom.opening_quote import *
 
 def q_mult(q1, q2):
     w1, x1, y1, z1 = q1
